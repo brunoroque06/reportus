3fea1ffaba4891f6648f436ae8a945f68530395b
Bruno Roque <brunoroque06@gmail.com>
2026-03-17 22:35:19 +0100

    Add working (?) stlite

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ from os import path
 
 import streamlit as st
 
+
 st.set_page_config(
-    initial_sidebar_state="expanded", page_icon=":sparkles:", page_title="Reportus"
+    initial_sidebar_state="expanded",
+    layout="wide",
+    page_title="Reportus",
 )
 
 
-def page(file: str, title: str):
-    return st.Page(path.join("src", "page", file), title=title)
+def pages():
+    return [
+        ("dtvp3.py", "DTVP-3"),
+        ("dtvpa.py", "DTVP-A"),
+        ("mabc.py", "MABC"),
+        ("spm.py", "SPM"),
+    ]
 
 
-pages = [
-    ("dtvp3.py", "DTVP-3"),
-    ("dtvpa.py", "DTVP-A"),
-    ("mabc.py", "MABC"),
-    ("spm.py", "SPM"),
-]
-pages = [page(file, title) for file, title in pages]
-
-pg = st.navigation(pages)
+pg = st.navigation(
+    [st.Page(path.join("src", "page", file), title=title) for file, title in pages()]
+)
 
 pg.run()
