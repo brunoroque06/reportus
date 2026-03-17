a76840b20c2e0ed2d0724141912adfe5ce17ae6d
Bruno Roque <brunoroque06@gmail.com>
2026-03-17 23:52:43 +0100

    SPM text is on the left

--- src/page/spm.py
+++ src/page/spm.py
@@ -7,9 +7,9 @@ from src.report import spm
 
 
 def page():
-    left_col, right_col = ui.structure("SPM")
+    left, right = ui.structure("SPM")
 
-    with left_col:
+    with left:
         cols = st.columns(4)
         today = datetime.date.today()
 
@@ -32,22 +32,22 @@ def page():
 
         scores = spm.get_scores()
 
-        left = (
+        left_forms = (
             ["soc", "vis", "hea"]
             if ver1()
             else ["vis", "hea", "tou", "t&s", "bod", "bal"]
         )
-        right = ["tou", "t&s", "bod", "bal", "pln"] if ver1() else ["pln", "soc"]
+        right_forms = ["tou", "t&s", "bod", "bal", "pln"] if ver1() else ["pln", "soc"]
 
         raw: dict[str, int] = {}
 
         cols = st.columns(2)
         with cols[0]:
-            for s in left:
+            for s in left_forms:
                 raw[s] = st.number_input(scores[s], step=1)
 
         with cols[1]:
-            for s in right:
+            for s in right_forms:
                 raw[s] = st.number_input(scores[s], step=1)
 
         name = None
@@ -57,7 +57,7 @@ def page():
                 name = st.text_input("Name")
 
         res, rep = spm.process(asmt, form, ver, filer, name, raw)
-
-    with right_col:
         ui.text(rep)
+
+    with right:
         ui.table(res)
