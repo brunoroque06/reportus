e7729f5897303cb3b8757d157674d33c099d4846
Bruno Roque <brunoroque06@gmail.com>
2026-03-28 09:59:24 +0100

    main.py -> app.py

--- e2e/test_pages.py
+++ e2e/test_pages.py
@@ -1,11 +1,11 @@
 import pytest
 from streamlit.testing.v1 import AppTest
 
-from main import tabs
+from app import tabs
 
 
 @pytest.mark.parametrize("rep", [t[0] for t in tabs()])
 def test_report(rep: str):
-    at = AppTest.from_file("main.py").run()
+    at = AppTest.from_file("app.py").run()
     tab = next((t for t in at.tabs if t.label == rep))
     assert len(tab.code[0].value) > 0
